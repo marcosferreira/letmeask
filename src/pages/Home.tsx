486ac65e0e7a93d33257bd8b50a8e9112e8bfa3e
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-06-23 17:43:01 -0300

    finish screen Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
-import IllustrationSVG from '../assets/images/illustration.svg';
-import LogoSVG from '../assets/images/logo.svg';
-import GoogleIconSVG from '../assets/images/google-icon.svg';
+import { Button } from "../components/Button";
+
+import IllustrationSVG from "../assets/images/illustration.svg";
+import LogoSVG from "../assets/images/logo.svg";
+import GoogleIconSVG from "../assets/images/google-icon.svg";
+
+import "../styles/auth.scss";
 
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img
           src={IllustrationSVG}
@@ -14,21 +18,16 @@ export function Home() {
         <p>Tire as dúvidas da sua audiencia em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={LogoSVG} alt="Letmeask" />
-          <button>
+          <button className="btn-create-room">
             <img src={GoogleIconSVG} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
-            <input
-              type="text"
-              placeholder="Digite o código da sala"
-            />
-            <button type="submit">
-              Entra na sala
-            </button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entra na sala</Button>
           </form>
         </div>
       </main>
